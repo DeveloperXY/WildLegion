92a43dcef109dab0dc511453c0cffd47cbb99ffd
unknown <m.a.zouag@gmail.com>
2018-09-08 16:08:04 +0100

    Play music in background.

--- app/src/main/java/com/developerxy/wildlegion/App.kt
+++ app/src/main/java/com/developerxy/wildlegion/App.kt
@@ -1,11 +1,34 @@
 package com.developerxy.wildlegion
 
 import android.app.Application
+import android.media.MediaPlayer
 import android.support.v7.app.AppCompatDelegate
 
-class App : Application() {
+class App : Application(), LifeCycleDelegate {
+
+    lateinit var mediaPlayer: MediaPlayer
+
     override fun onCreate() {
         super.onCreate()
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
+        val lifeCycleHandler = AppLifecycleHandler(this)
+        registerLifecycleHandler(lifeCycleHandler)
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.wl_bg_music)
+        mediaPlayer.isLooping = true
+        mediaPlayer.start()
+    }
+
+    override fun onAppBackgrounded() {
+        mediaPlayer.pause()
+    }
+
+    override fun onAppForegrounded() {
+        mediaPlayer.start()
+    }
+
+    private fun registerLifecycleHandler(lifeCycleHandler: AppLifecycleHandler) {
+        registerActivityLifecycleCallbacks(lifeCycleHandler)
+        registerComponentCallbacks(lifeCycleHandler)
     }
 }
