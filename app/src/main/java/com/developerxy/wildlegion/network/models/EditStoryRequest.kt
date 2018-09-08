0ad36ac0b873d06876e9732183ea38ef95feaa86
unknown <m.a.zouag@gmail.com>
2018-09-08 11:41:57 +0100

    Fix a bug causing edited news to have no original posters.

--- app/src/main/java/com/developerxy/wildlegion/network/models/EditStoryRequest.kt
+++ app/src/main/java/com/developerxy/wildlegion/network/models/EditStoryRequest.kt
@@ -6,6 +6,7 @@ data class EditStoryRequest(
         var _id: String,
         var title: String,
         var postDate: String,
+        var postedBy: String,
         var newsStory: String) {
-    constructor(news: News) : this(news._id, news.title, news.postDate, news.newsStory)
+    constructor(news: News) : this(news._id, news.title, news.postDate, news.postedBy, news.newsStory)
 }
